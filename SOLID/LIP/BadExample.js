60f06decc4a05be908fadd2d33671bb053ca95b9
Ajmal Nasumudeen <ajmal.nasumudeen@oceanai.ai>
2024-04-04 12:59:56 +0530

    lip

---
+++ SOLID/LIP/BadExample.js
@@ -0,0 +1,22 @@
+class Bird {
+
+}
+
+class FlyingBird {
+
+}
+
+class NonFlyingBird {
+
+}
+
+class NonFlyingSwim extends NonFlyingBird {
+    // interface of swim or 
+}
+
+class FlyIngSwim extends FlyingBird {
+
+}
+
+// the issue is above code in class explosion
+// we are not able to get a common type for a behavior
---
+++ SOLID/LIP/LIP_Example.ts
@@ -0,0 +1,52 @@
+// Base class representing a bird
+abstract class BirdC {
+    constructor(name: string, height: number) {
+
+    }
+}
+
+
+interface Flyable {
+    fly(): void
+}
+
+
+interface Sound {
+    makeSound(): void
+}
+
+
+
+
+
+//  [+++++++++++++++++++++++++++++]
+
+class Pigeon extends BirdC implements Flyable, Sound {
+    fly(): void {
+        console.log('Pigeon is flying.');
+    }
+
+    makeSound(): void {
+        console.log('Pigeon is cooing.');
+    }
+}
+
+
+class CrowC extends Bird implements Flyable, Sound {
+    makeSound(): void {
+        console.log('Crow is cawing.');
+    }
+    fly(): void {
+        console.log('Crow is flying.');
+    }
+}
+
+// Client code
+function makeBirdSound(bird: BirdC): void {
+}
+
+const sparrow = new Sparrow();
+const crow = new Crow();
+
+makeBirdSound(sparrow); // Output: Sparrow is chirping.
+makeBirdSound(crow);    // Output: Crow is cawing.
